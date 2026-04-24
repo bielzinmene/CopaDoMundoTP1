473e6b155881ad7b52f658428a81ca7d0e7dbb00
Gabriel Menezes <gabrielmenezes.tais@gmail.com>
2026-04-24 12:46:08 -0300

    Criando a pasta src e o pacote br.unb.cic.copa.model com as classes Jogador e Selecao embutidas.

---
+++ src/br/unb/cic/copa/model/Jogador.java
@@ -0,0 +1,57 @@
+package br.unb.cic.copa.model;
+
+public class Jogador {
+    private String nome;
+    private int numeracao;
+    private String posicao;
+    private String selecao;
+    private boolean titular;
+
+    public Jogador(String nome, int numeracao, String posicao, String selecao, boolean titular) {
+        this.nome = nome;
+        this.numeracao = numeracao;
+        this.posicao = posicao;
+        this.selecao = selecao;
+        this.titular = titular;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNumeracao(int numeracao) {
+        this.numeracao = numeracao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public void setSelecao(String selecao) {
+        this.selecao = selecao;
+    }
+
+    public void setTitular(boolean titular) {
+        this.titular = titular;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumeracao() {
+        return numeracao;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public String getSelecao() {
+        return selecao;
+    }
+
+    public boolean isTitular() {
+        return titular;
+    }
+}
---
+++ src/br/unb/cic/copa/model/Selecao.java
@@ -0,0 +1,50 @@
+package br.unb.cic.copa.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Selecao {
+    private String nome;
+    private String grupo;
+    private String tecnico;
+    private List<Jogador> jogadores;
+
+    public Selecao(String nome, String grupo, String tecnico) {
+        this.nome = nome;
+        this.grupo = grupo;
+        this.tecnico = tecnico;
+        this.jogadores = new ArrayList<>();
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public void setJogadores(List<Jogador> jogadores) {
+        this.jogadores = jogadores;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public String getTecnico() {
+        return tecnico;
+    }
+
+    public List<Jogador> getJogadores() {
+        return jogadores;
+    }
+}
