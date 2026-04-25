03c9321e855838fc15c21c4f486812e56c60c351
Esthefany Torres <212005382@aluno.unb.br>
2026-04-25 20:14:51 -0300

    Adicionando estrutura inicial do Estadio e Arbitro

---
+++ src/br/unb/cic/copa/model/Arbitro.java
@@ -0,0 +1,41 @@
+package br.unb.cic.copa.model;
+
+public class Arbitro {
+
+    private String nome;
+    private String nacionalidade;
+    private int experiencia;
+
+    public Arbitro(String nome, String nacionalidade, int experiencia) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.experiencia = experiencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade){
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia){
+        this.experiencia = experiencia;
+    }
+
+
+
+}
