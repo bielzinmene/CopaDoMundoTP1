ceb2c548c263132bfc04c508340a16dc0ae9cf4f
Esthefany Torres <212005382@aluno.unb.br>
2026-05-11 16:57:20 -0300

    Adicionando estrutura inicial do Estadio e Arbitro

--- src/br/unb/cic/copa/model/aluno3/Localizacao.java
+++ src/br/unb/cic/copa/model/aluno3/Localizacao.java
@@ -3,11 +3,13 @@ package br.unb.cic.copa.model.aluno3;
 public class Localizacao {
     private String cidade;
     private String estado;
+    private String pais;
     private String endereco;
 
-    public Localizacao(String cidade, String estado, String endereco) {
+    public Localizacao(String cidade, String estado, String pais, String endereco) {
         this.cidade = cidade;
         this.estado = estado;
+        this.pais = pais;
         this.endereco = endereco;
     }
 
@@ -16,6 +18,10 @@ public class Localizacao {
     }
 
     public String getEstado() {
+        return estado;
+    }
+
+    public String getPais() {
         return estado;
     }
 
