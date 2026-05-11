84448f41d4fed118b038abf655e94cdc9b0edd45
Esthefany Torres <212005382@aluno.unb.br>
2026-05-11 16:45:06 -0300

    Adicionando estrutura inicial do Estadio e Arbitro

--- src/br/unb/cic/copa/model/aluno3/Arbitro.java
+++ src/br/unb/cic/copa/model/aluno3/Arbitro.java
@@ -1,15 +1,22 @@
 package br.unb.cic.copa.model.aluno3;
 
-public class Arbitro {
+import java.util.ArrayList;
+import java.util.List;
+import br.unb.cic.copa.model.aluno4.Partida;
 
+public class Arbitro {
+    //atributos
     private String nome;
     private String nacionalidade;
     private int experiencia;
+    private List<Partida> partidasDesignadas;
 
+        //Construtor
     public Arbitro(String nome, String nacionalidade, int experiencia) {
         this.nome = nome;
         this.nacionalidade = nacionalidade;
         this.experiencia = experiencia;
+        this.partidasDesignadas = new ArrayList<>();
     }
 
     public String getNome() {
@@ -24,7 +31,8 @@ public class Arbitro {
         return nacionalidade;
     }
 
-    public void setNacionalidade(String nacionalidade){
+    public void setNacionalidade(String nacionalidade)
+    {
         this.nacionalidade = nacionalidade;
     }
 
@@ -35,6 +43,21 @@ public class Arbitro {
     public void setExperiencia(int experiencia){
         this.experiencia = experiencia;
     }
+
+    public List<Partida>consultarEscala(){
+        return partidasDesignadas;
+    }
+
+    ///public boolean validarNacionalidade(Partida partida){
+        ///return  !this.nacionalidade.equalsIgnoreCase(partida.getSelecao1())
+          ///      && !this.nacionalidade.equalsIgnoreCase(partida.getSelecao2());
+    ///}
+
+    ///public void addPartidaAEscala( Partida partida){
+      ///  if(validarNacionalidade(partida)){
+         ///   this.partidasDesignadas.add(partida);
+        ///}
+    ///}
 
 
 
--- src/br/unb/cic/copa/model/aluno3/Estadio.java
+++ src/br/unb/cic/copa/model/aluno3/Estadio.java
@@ -1,15 +1,21 @@
 package br.unb.cic.copa.model.aluno3;
 
+import java.util.ArrayList;
+import java.util.List;
+import br.unb.cic.copa.model.aluno4.Partida;
+
 public class Estadio {
 
     private String nome;
     private String localizacao;
     private int capacidade;
+    private List<Partida> partidasSediadas;
 
     public Estadio(String nome, String localizacao, int capacidade) {
         this.nome = nome;
         this.localizacao = localizacao;
         this.capacidade = capacidade;
+        this.partidasSediadas = new ArrayList<>();
     }
 
     public String getNome() {
@@ -35,4 +41,24 @@ public class Estadio {
     public void setCapacidade(int capacidade){
         this.capacidade = capacidade;
     }
+
+    public List<Partida> getPartidasSediadas(){
+        return partidasSediadas;
+    }
+
+    /* public boolean verificarDisponibilidade(String data){
+        for (Partida p: partidasSediadas){
+            if (p.getData().equals(data)){
+                return false;
+            }
+        }
+        return true;
+    } */
+
+    /* public void adicionarPartida(Partida partida){
+        if (verificarDisponibilidade(partida.getData())){
+            this.partidasSediadas.add(partida);
+        }
+    } */
+
 }
