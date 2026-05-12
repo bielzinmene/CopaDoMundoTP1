acedfa5c1578d4fd15d3ee50b2848306e9f825e3
MarquesFernanda <fernandamarques1411@gmail.com>
2026-05-12 11:19:32 -0300

    Mudança no Botão de Gerenciar Partidas

--- src/br/unb/cic/copa/model/aluno1/Usuario.java
+++ src/br/unb/cic/copa/model/aluno1/Usuario.java
@@ -11,87 +11,16 @@ public abstract class Usuario {
     protected String funcao;
     protected String status;
 
-    public Usuario(int id, String nome, String email, String login, String senha, String cpf, String pais, String funcao) {
-        this.id = id;
+    public Usuario(String nome, String email, String login, String senha, String identificacao, String pais, String funcao) {
         this.nome = nome;
         this.email = email;
         this.login = login;
         this.senha = senha;
-        this.cpf = cpf;
+        this.identificacao = identificacao;
         this.pais = pais;
         this.funcao = funcao;
         this.status = "Ativo";
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public String getFuncao() {
-        return funcao;
-    }
-
-    public void setFuncao(String funcao) {
-        this.funcao = funcao;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    
 }
