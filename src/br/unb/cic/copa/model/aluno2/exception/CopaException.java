947d6580f5f95f25d64275112c92c78677634b65
Gabriel Menezes <gabrielmenezes.tais@gmail.com>
2026-06-01 14:05:20 -0300

    criação do package exception para exceções, criação do enum StatusJogador e lançamento de exceções na classe jogador

---
+++ src/br/unb/cic/copa/model/aluno2/exception/CopaException.java
@@ -0,0 +1,8 @@
+package br.unb.cic.copa.model.aluno2.exception;
+
+public class CopaException extends Exception {
+    public CopaException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/br/unb/cic/copa/model/aluno2/exception/NumeroCamisaInvalidoException.java
@@ -0,0 +1,8 @@
+package br.unb.cic.copa.model.aluno2.exception;
+
+public class NumeroCamisaInvalidoException extends CopaException {
+    public NumeroCamisaInvalidoException(String message) {
+        super(message);
+    }
+}
+
